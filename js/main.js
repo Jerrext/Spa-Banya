7ed860a34f5b30f281883dda472b70a865b3d281
Daniil <kolpakov_2002@mail.ru>
2024-05-19 14:07:02 +0300

    finally fix content, clear code

--- js/main.js
+++ js/main.js
@@ -8,44 +8,6 @@ const overflowToggle = (arg) => {
   }
 };
 
-// const popUpOpen = (window, wrapper, overlay, closeBtn, delay) => {
-//   overflowToggle(false);
-//   window.style.display = "block";
-
-//   setTimeout(() => {
-//     overlay.style.opacity = ".8";
-//     wrapper.style.opacity = "1";
-//     wrapper.style.top = "50%";
-//     closeBtn.style.opacity = "0.5";
-//   }, 0);
-
-//   if (delay) {
-//     setTimeout(() => {
-//       popUpClose(window, wrapper, overlay);
-//     }, delay);
-//   }
-// };
-
-// const popUpClose = (window, wrapper, overlay, closeBtn) => {
-//   setTimeout(() => {
-//     window.style.display = "";
-//   }, 300);
-//   closeBtn.style.opacity = "";
-//   overlay.style.opacity = "";
-//   wrapper.style.opacity = "";
-//   wrapper.style.top = "";
-
-//   overflowToggle(true);
-// };
-
-// const setPopUpVisibility = (visibility, window, wrapper, overlay, closeBtn, delay) => {
-//   if (visibility) {
-//     popUpOpen(window, wrapper, overlay, closeBtn, delay);
-//   } else {
-//     popUpClose(window, wrapper, overlay, closeBtn);
-//   }
-// };
-
 document.addEventListener("DOMContentLoaded", () => {
   // Header
 
@@ -155,104 +117,6 @@ document.addEventListener("DOMContentLoaded", () => {
   stagesResizeHandler();
 
   window.addEventListener("resize", stagesResizeHandler);
-
-  // Map
-
-  // const map = document.querySelector(".about__map iframe");
-  // map.style.width = "100%";
-  // map.style.height = "100%";
-
-  // Services
-
-  // document.querySelectorAll(".services__item").forEach((item,index) => item.dataset.number = index + 1)
-
-  // Popup View
-
-  // const popupView = document.querySelector(".popup-view")
-  // const popupViewIWindow = document.querySelector(".popup-view__window")
-  // const overlayView = document.querySelector(".popup-view__overlay")
-  // const closeBtnView = document.querySelector(".popup-view__close")
-  // const prevBtnView = document.querySelector(".popup-view__prev")
-  // const nextBtnView = document.querySelector(".popup-view__next")
-
-  // const img = document.querySelector(".popup-view__img")
-  // const imgTitle = document.querySelector(".popup-view__title")
-  // const imgDescription = document.querySelector(".popup-view__description")
-  // const parentTextContainer = imgTitle.parentElement
-  // const parentImgContainer = img.parentElement
-
-  // const galleryItems = [...document.querySelectorAll(".gallery__grid-item")]
-
-  // const galleryList = galleryItems.map((item, index)=> {
-  //   return {
-  //     id: index,
-  //     img: item.firstElementChild.src,
-  //     title: item.firstElementChild.nextElementSibling.textContent,
-  //     description: item.lastElementChild.innerHTML,
-  //   }
-  // })
-
-  // window.addEventListener('popstate', function(e) {
-  //   setPopUpVisibility(false, popupView, popupViewIWindow, overlayView, closeBtnView)
-  //   burgerMenuClose()
-  // }, false);
-
-  // let currentViewIndex
-
-  // const openViewWindow = (id) => {
-  //   if (id === 0) {
-  //     prevBtnView.style.display = "none"
-  //   } else if (id < 0) {
-  //     return
-  //   } else if (id === galleryList.length - 1) {
-  //     nextBtnView.style.display = "none"
-  //   } else if (id > galleryList.length - 1) {
-  //     return
-  //   } else {
-  //     prevBtnView.style.display = "flex"
-  //     nextBtnView.style.display = "flex"
-  //   }
-
-  //   currentViewIndex = id
-
-  //   const currentItem = galleryList.find(work => work.id === id)
-
-  //   if (!currentItem.title && !currentItem.description) {
-  //     popupViewIWindow.style.width = "unset"
-  //     parentTextContainer.style.display = "none"
-  //     if (document.body.clientWidth < 600) parentImgContainer.style.maxHeight = ""
-  //   } else {
-  //     parentTextContainer.style.display = "block"
-  //     if (document.body.clientWidth < 600) parentImgContainer.style.maxHeight = "400px"
-  //     parentTextContainer.style.padding = "50px 20px"
-  //     popupViewIWindow.style.width = "100%"
-  //   }
-
-  //   img.src = currentItem.img
-  //   imgTitle.textContent = currentItem.title
-  //   imgDescription.innerHTML = currentItem.description
-  // }
-
-  // const closeViewWindow = () => {
-  //   window.history.back();
-  //   setPopUpVisibility(false, popupView, popupViewIWindow, overlayView, closeBtnView)
-  // }
-
-  // prevBtnView.addEventListener("click", () => openViewWindow(currentViewIndex - 1))
-  // nextBtnView.addEventListener("click", () => openViewWindow(currentViewIndex + 1))
-
-  // galleryItems.forEach((item, index) => {
-  //   item.dataset.index = index
-
-  //   item.addEventListener("click", () => {
-  //     openViewWindow(+item.dataset.index)
-  //     window.history.pushState({popup: 1}, "popup","#popup")
-  //     setPopUpVisibility(true, popupView, popupViewIWindow, overlayView, closeBtnView)
-  //   })
-  // })
-
-  // closeBtnView.addEventListener("click", closeViewWindow)
-  // overlayView.addEventListener("click", closeViewWindow)
 
   // Burger
 
@@ -403,10 +267,6 @@ const sponsorsSwiper = new Swiper(".sponsors__swiper", {
 const laznyaSwiper = new Swiper(".laznya__swiper", {
   spaceBetween: 20,
   slidesPerView: 1,
-  // navigation: {
-  //   nextEl: ".laznya .swiper-button-next",
-  //   prevEl: ".laznya .swiper-button-prev",
-  // },
   loop: true,
   simulateTouch: true,
   pagination: {
